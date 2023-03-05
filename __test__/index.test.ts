94372e02850a11a0dca43c66a5b048ff19b2fb3e
Tony Edgal <tonyedgal@gmail.com>
2023-03-05 23:26:31 +0100

    test: updated test requirements

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -5,7 +5,7 @@ describe("streakCounter", () => {
   let mockLocalStorage: Storage;
 
   beforeEach(() => {
-    const mockJSDom = new JSDOM("", { url: "http://localhost" });
+    const mockJSDom = new JSDOM("", { url: "https://localhost" });
 
     mockLocalStorage = mockJSDom.window.localStorage;
   });
@@ -17,5 +17,22 @@ describe("streakCounter", () => {
     expect(streak.hasOwnProperty("currentCount")).toBe(true);
     expect(streak.hasOwnProperty("startDate")).toBe(true);
     expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
+  });
+
+  it("should return a streak starting at 1 and keep track of lastLogindate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    function formattedDate(date: Date): string {
+      // return date as 11/11/2021
+      // other times it returns 11/11/2021, 12:00:00 AM
+      // which is why we call the .split at the end
+      return date.toLocaleDateString("en.US");
+    }
+
+    const dateFormatted = formattedDate(date);
+
+    expect(streak.currentCount).toBe(1);
+    expect(streak.lastLoginDate).toBe(dateFormatted);
   });
 });
