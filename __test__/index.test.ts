71d7d766cf2e27ead13b2154ce3cf4a181818d49
Tony Edgal <tonyedgal@gmail.com>
2023-03-05 23:38:05 +0100

    fix: fixed error in test and `formattedDate` func

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -27,7 +27,7 @@ describe("streakCounter", () => {
       // return date as 11/11/2021
       // other times it returns 11/11/2021, 12:00:00 AM
       // which is why we call the .split at the end
-      return date.toLocaleDateString("en.US");
+      return date.toLocaleDateString("en-US");
     }
 
     const dateFormatted = formattedDate(date);
--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ export function formattedDate(date: Date): string {
   // return date as 11/11/2021
   // other times it returns 11/11/2021, 12:00:00 AM
   // which is why we call the .split at the end
-  return date.toLocaleDateString("en.US");
+  return date.toLocaleDateString("en-US");
 }
