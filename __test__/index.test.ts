e415c13835c18529a8576abe200aee4caf3b3ebb
Tony Edgal <tonyedgal@gmail.com>
2023-03-05 22:12:15 +0100

    feat: write initial tests

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,5 +1,11 @@
-describe("basic test", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+describe("streakCounter", () => {
+  it(" should return a streak object with currentCount, startDate and lastLoginDate", () => {
+    const mockLocalStorage = "";
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    expect(streak.hasOwnProperty("currentCount")).toBe(true);
+    expect(streak.hasOwnProperty("startDate")).toBe(true);
+    expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
   });
 });
