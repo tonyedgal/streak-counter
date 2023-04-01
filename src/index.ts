2973a7af5f062cb98479d65105c64f0d3aa72833
Tony Edgal <tonyedgal@gmail.com>
2023-04-01 22:05:47 +0100

    Feat: store streak in local storage

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,18 @@ interface Streak {
   lastLoginDate: string;
 }
 
+// used when storing in local storage
+const KEY = "streak";
+
 export function streakCounter(storage: Storage, date: Date): Streak {
-  return {
+  const streak = {
     currentCount: 1,
     startDate: formattedDate(date),
     lastLoginDate: formattedDate(date),
   };
+
+  // store in local storage
+  storage.setItem(KEY, JSON.stringify(streak));
+
+  return streak;
 }
