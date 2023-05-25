5625ad1e1b96af0e69a20281e92f795fcd256d3e
tonyedgal <tonyedgal@gmail.com>
2023-05-25 22:35:15 +0100

    feat: func to reset or continue streak

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,40 @@ interface Streak {
 // used when storing in local storage
 const KEY = "streak";
 
+export function differenceInDays(dateLeft: Date, dateRight: Date): number {
+  const diffTIme = Math.abs(dateLeft.getTime() - dateRight.getTime());
+  const differenceInDays = Math.ceil(diffTIme / (1000 * 60 * 60 * 24));
+
+  return differenceInDays;
+}
+
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): "increment" | undefined {
+  // we get 11/5/2021
+  // so to get 5, we use oue helper function
+  const difference = differenceInDays(currentDate, new Date(lastLoginDate));
+  // this means they logged in the day after the currentDate
+  if (difference === 1) {
+    return "increment";
+  }
+  // otherwise they logged in after a day,
+  // which would break the streak
+
+  return undefined;
+}
+
 export function streakCounter(storage: Storage, date: Date): Streak {
   // check if streak exists in local storage
   const streakInLocalStorage = storage.getItem(KEY);
   if (streakInLocalStorage) {
     try {
       const streak = JSON.parse(streakInLocalStorage);
-      const state = "increment";
+      const state = shouldIncrementOrResetStreakCount(
+        date,
+        streak.lastLoginDate
+      );
       const SHOULD_INCREMENT = state === "increment";
 
       if (SHOULD_INCREMENT) {
