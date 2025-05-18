3e6e49545513bf6c3292e8c95d034bfe9b367bc2
tonyedgal <tonyedgal@gmail.com>
2025-05-18 23:19:10 +0100

    feat: add test to ensure streak is not reset for same-day login

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -141,4 +141,16 @@ describe("with a pre-populated streak", () => {
 
     expect(streak.currentCount).toBe(1);
   });
+
+  it("should not reset the streak for same-day login", () => {
+    const date = new Date("12/13/2021");
+    // Call it once so it updates the streak
+    streakCounter(mockLocalStorage, date);
+
+    // Simulate same-day login
+    const dateUpdated = new Date("12/13/2021");
+    const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+    expect(streakUpdated.currentCount).toBe(2);
+  });
 });
